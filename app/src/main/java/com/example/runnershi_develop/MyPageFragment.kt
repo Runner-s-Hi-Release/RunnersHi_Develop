745989a9297087b9a6989fece612a59495008e0f
chaeppy <chaeppy108@gmail.com>
2020-09-04 05:36:14 +0900

    Add MyPageFragment.kt

---
+++ app/src/main/java/com/example/runnershi_develop/MyPageFragment.kt
@@ -0,0 +1,19 @@
+package com.example.runnershi_develop
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.runnershi_develop.databinding.FragmentMyPageBinding
+import com.example.runnershi_develop.databinding.FragmentRankBinding
+import com.example.runnershi_develop.databinding.FragmentViewPagerBinding
+
+class MyPageFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentMyPageBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
