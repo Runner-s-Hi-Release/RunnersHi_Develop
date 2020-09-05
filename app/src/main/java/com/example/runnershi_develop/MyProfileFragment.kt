36fad37195d3befed5fe3312a49f238b5645c999
famer9716 <famer3625@gmail.com>
2020-09-05 23:21:03 +0900

    Add MyProfileFragment.kt

--- app/src/main/java/com/example/runnershi_develop/MyProfileFragment.kt
+++ app/src/main/java/com/example/runnershi_develop/MyProfileFragment.kt
@@ -5,13 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.example.runnershi_develop.adapters.MyProfileBadgeAdapter
 import com.example.runnershi_develop.databinding.FragmentMyProfileBinding
+import com.example.runnershi_develop.utilities.InjectorUtils
+import com.example.runnershi_develop.viewmodels.MyProfileViewModel
+import androidx.lifecycle.observe
 
 class MyProfileFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val binding = FragmentMyProfileBinding.inflate(inflater, container, false)
+
+    private val myProfileViewModel: MyProfileViewModel by viewModels{
+        InjectorUtils.provideMyProfileViewModelFactory(requireContext())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentMyProfileBinding>(
+            inflater, R.layout.fragment_my_profile, container, false
+        ).apply{
+            viewModel = myProfileViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        val adapter = MyProfileBadgeAdapter()
+
+        binding.btnLogout.setOnClickListener{
+            myProfileViewModel.deleteUser()
+        }
+
+        binding.rvBadge.adapter =adapter
+
+        subscribeUi(adapter)
 
         return binding.root
+    }
+
+    private fun subscribeUi(adapter: MyProfileBadgeAdapter) {
+        myProfileViewModel.user.observe(viewLifecycleOwner) { result ->
+            when(result){
+                null -> adapter.submitList(null)
+                else -> adapter.submitList(result.badges)
+            }
+        }
     }
 }
