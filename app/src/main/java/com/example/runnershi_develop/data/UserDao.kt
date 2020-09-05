c74b35d65a022884a4c75b0ad0fe111ad6ec48c7
famer9716 <famer3625@gmail.com>
2020-09-05 21:08:50 +0900

    Add UserDao.kt interface

---
+++ app/src/main/java/com/example/runnershi_develop/data/UserDao.kt
@@ -0,0 +1,16 @@
+package com.example.runnershi_develop.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user LIMIT 1")
+    fun getUsers(): LiveData<User?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: User)
+
+    @Delete
+    suspend fun deleteUser(user: User)
+}
