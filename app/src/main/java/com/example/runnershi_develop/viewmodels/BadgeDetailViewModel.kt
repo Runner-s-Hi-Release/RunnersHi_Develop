8f88ba9324f692f29bbec29b4f75f4d7cfa68ae1
famer9716 <famer3625@gmail.com>
2020-09-05 21:33:13 +0900

    Add BadgeDetailViewModel.kt

---
+++ app/src/main/java/com/example/runnershi_develop/viewmodels/BadgeDetailViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.runnershi_develop.viewmodels
+
+import androidx.lifecycle.*
+import com.example.runnershi_develop.BuildConfig
+import com.example.runnershi_develop.data.BadgeDetail
+import com.example.runnershi_develop.data.BadgeDetailRepository
+import kotlinx.coroutines.launch
+
+class BadgeDetailViewModel internal constructor(
+    private val badgeDetailRepository: BadgeDetailRepository,
+    val index: Int
+) : ViewModel() {
+    val badgeDetail: MutableLiveData<BadgeDetail> = MutableLiveData()
+
+    init{
+        viewModelScope.launch {
+            badgeDetail.postValue(getBadgeDetail())
+        }
+    }
+
+    private suspend fun getBadgeDetail(): BadgeDetail?{
+        return badgeDetailRepository.getBadgeDetail(BuildConfig.USER_ACCESS_KEY, index)
+    }
+
+}
