e4eaa3736194508e241ccd37577df30de74848e7
famer9716 <famer3625@gmail.com>
2020-09-05 21:09:22 +0900

    Add AppDatabase.kt

---
+++ app/src/main/java/com/example/runnershi_develop/data/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.runnershi_develop.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.runnershi_develop.utilities.DATABASE_NAME
+
+@TypeConverters(Converters::class)
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+                    .build()
+        }
+    }
+}
