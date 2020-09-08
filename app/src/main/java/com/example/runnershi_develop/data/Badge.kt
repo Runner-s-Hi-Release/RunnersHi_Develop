d7e0154d6aec2925a54d86dc371de59045b6bed2
famer9716 <famer3625@gmail.com>
2020-09-08 17:15:00 +0900

    Fix BadgeItemViewModel.kt to Badge.kt data class

---
+++ app/src/main/java/com/example/runnershi_develop/data/Badge.kt
@@ -0,0 +1,6 @@
+package com.example.runnershi_develop.data
+
+data class Badge(
+    val badge: Boolean,
+    val index: Int
+)
--- app/src/main/java/com/example/runnershi_develop/viewholders/BadgeViewHolder.kt
+++ app/src/main/java/com/example/runnershi_develop/viewholders/BadgeViewHolder.kt
@@ -5,22 +5,25 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.runnershi_develop.HomeViewPagerFragmentDirections
 import com.example.runnershi_develop.databinding.ListItemBadgeBinding
-import com.example.runnershi_develop.viewmodels.BadgeItemViewModel
+import com.example.runnershi_develop.data.Badge
 
 class BadgeViewHolder(private val binding: ListItemBadgeBinding) : RecyclerView.ViewHolder(binding.root) {
 
     init{
         binding.setClickListener { view ->
-            binding.viewModel?.let { viewModel ->
-                if(viewModel.badge)
-                    navigateToPlant(viewModel.index, view)
+            binding.badgeData?.let { badgeData ->
+                if(badgeData.badge)
+                    navigateToPlant(badgeData.index, view)
             }
         }
     }
 
     fun bind(badgeBool: Boolean, index: Int) {
         with(binding) {
-            viewModel= BadgeItemViewModel(badgeBool, index)
+            badgeData= Badge(
+                badgeBool,
+                index
+            )
             executePendingBindings()
         }
     }
