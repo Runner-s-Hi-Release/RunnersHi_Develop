8d0cd5e10646ae72f1b7c3927efed58e2e2918fa
RoadtoS7 <kshvv975@gmail.com>
2020-09-09 22:40:06 +0900

    Add requestRecord function 모든 러닝기록을 서버에 요청하는 함수를 구현함

--- app/src/main/java/com/example/runnershi_develop/api/RequestInterface.kt
+++ app/src/main/java/com/example/runnershi_develop/api/RequestInterface.kt
@@ -1,6 +1,7 @@
 package com.example.runnershi_develop.api
 
 import com.example.runnershi_develop.data.*
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -15,4 +16,6 @@ interface RequestInterface {
     suspend fun requestBadgeDetail(@Header("token") token : String, @Path("flag") flag : Int)
             : Response<ResponseData<BadgeDetail>>
 
+    @GET("record/all")
+    fun requestRecord(@Header("token") token : String) : Call<ResponseData<Record>>
 }
