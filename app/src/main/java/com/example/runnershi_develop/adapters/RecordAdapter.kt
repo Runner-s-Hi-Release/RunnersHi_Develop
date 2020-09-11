48aa82a63d33f5e65bd21c4624b9282bb5ce7e2a
RoadtoS7 <kshvv975@gmail.com>
2020-09-11 18:38:00 +0900

    Add RecordAdapter.kt for RecyclerView in RecordFragment.kt 모든 기록을 나타낼 RecyclerView에 대한 Adapter를 만듬

---
+++ app/src/main/java/com/example/runnershi_develop/adapters/RecordAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.runnershi_develop.adapters
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.runnershi_develop.data.Running
+import com.example.runnershi_develop.databinding.ListItemRecordBinding
+
+
+class RecordAdapter(private val onClickListener: OnClickListener) :
+    ListAdapter<Running, RecordAdapter.ViewHolder>(DiffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListItemRecordBinding.inflate(
+            LayoutInflater.from(parent.context)
+        )
+        return ViewHolder(
+            ListItemRecordBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                )
+            ), onClickListener
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    class ViewHolder(
+        private var binding: ListItemRecordBinding,
+        private val onClickListener: OnClickListener
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(running: Running) {
+            binding.running = running
+            binding.clickListener = onClickListener
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Running>() {
+        override fun areItemsTheSame(oldItem: Running, newItem: Running): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Running, newItem: Running): Boolean {
+            return (oldItem.runIdx == newItem.runIdx
+                    && oldItem.gameIdx == newItem.gameIdx)
+        }
+    }
+
+    class OnClickListener(val clickListener: (running: Running) -> Unit) {
+        fun onClick(running: Running) = clickListener(running)
+    }
+}
