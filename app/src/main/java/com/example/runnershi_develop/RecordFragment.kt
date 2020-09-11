ccb00e24ea991d380e22e080ca829d8b70e3a58a
RoadtoS7 <kshvv975@gmail.com>
2020-09-11 18:49:11 +0900

    Add ViewModel, Adapter, onClickListener to RecordFragment.kt ViewModel과 연결했으며, RecyclerView Adapter와 연결하여 러닝 기록을 표시함 onClickListener를 구현하여 DetailFragment로 넘어가는 로직을 구현함

--- app/src/main/java/com/example/runnershi_develop/RecordFragment.kt
+++ app/src/main/java/com/example/runnershi_develop/RecordFragment.kt
@@ -1,16 +1,50 @@
 package com.example.runnershi_develop
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.runnershi_develop.adapters.RecordAdapter
 import com.example.runnershi_develop.databinding.FragmentRecordBinding
+import com.example.runnershi_develop.utilities.InjectorUtils
+import com.example.runnershi_develop.viewmodels.RecordViewModel
 
 class RecordFragment : Fragment() {
+    lateinit var viewModelAdapter: RecordAdapter
+
+    private val viewModel: RecordViewModel by viewModels {
+        InjectorUtils.provideRecordViewModelFactory(requireActivity())
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentRecordBinding.inflate(inflater, container, false)
+            .apply {
+                viewModel = viewModel
+                lifecycleOwner = this@RecordFragment
+            }
+
+        viewModelAdapter = RecordAdapter(RecordAdapter.OnClickListener{record ->
+            viewModel.displayRecordDetail(record)
+        })
+
+        binding.recordList.adapter =viewModelAdapter
+
+        viewModel.runnings.observe(viewLifecycleOwner, Observer {
+            viewModelAdapter.submitList(it)
+        })
+
+        viewModel.navigateToDetail.observe(viewLifecycleOwner, Observer {record ->
+            this.findNavController().navigate(
+                HomeViewPagerFragmentDirections
+                    .actionHomeViewPagerFragmentToRecordDetailFragment(record)
+            )
+            viewModel.displayRecordDetailDone()
+        })
 
         return binding.root
     }
