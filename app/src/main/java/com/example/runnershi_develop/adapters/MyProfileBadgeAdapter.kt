548f7942695f8757183bf1a514a41c4be3dc362b
famer9716 <famer3625@gmail.com>
2020-09-18 02:15:52 +0900

    Add BadgeViewHolder to MyProfileBadgeAdapter.kt

--- app/src/main/java/com/example/runnershi_develop/adapters/MyProfileBadgeAdapter.kt
+++ app/src/main/java/com/example/runnershi_develop/adapters/MyProfileBadgeAdapter.kt
@@ -5,14 +5,15 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.runnershi_develop.R
 import com.example.runnershi_develop.data.Badge
-import com.example.runnershi_develop.viewholders.BadgeViewHolder
+import com.example.runnershi_develop.databinding.ListItemBadgeBinding
+import com.example.runnershi_develop.utilities.OnClickListener
 
 class MyProfileBadgeAdapter(private val onClick: (badge: Badge?)-> Unit): ListAdapter<Boolean, BadgeViewHolder>(
     BadgeDiffCallback()
 ) {
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BadgeViewHolder {
         return BadgeViewHolder(
             DataBindingUtil.inflate(
@@ -25,7 +26,20 @@ class MyProfileBadgeAdapter(private val onClick: (badge: Badge?)-> Unit): ListAd
     override fun onBindViewHolder(holder: BadgeViewHolder, position: Int) {
         holder.bind(getItem(position), position)
     }
+}
 
+class BadgeViewHolder(private val binding: ListItemBadgeBinding, private val onClick: (badge: Badge?)-> Unit) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(badgeBool: Boolean, index: Int) {
+        with(binding) {
+            badgeData = Badge(
+                badgeBool,
+                index
+            )
+            onClickListener = OnClickListener<Badge>(onClick)
+            executePendingBindings()
+        }
+    }
 }
 
 private class BadgeDiffCallback : DiffUtil.ItemCallback<Boolean>() {
