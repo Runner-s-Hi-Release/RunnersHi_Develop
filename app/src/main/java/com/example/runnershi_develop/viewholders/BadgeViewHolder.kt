cc36b2555a771967e16a763b3fc9f3c7a438a29f
famer9716 <famer3625@gmail.com>
2020-09-18 02:09:55 +0900

    Add onClicklistener, onClick to BadgeViewHolder.kt

--- app/src/main/java/com/example/runnershi_develop/viewholders/BadgeViewHolder.kt
+++ app/src/main/java/com/example/runnershi_develop/viewholders/BadgeViewHolder.kt
@@ -1,36 +1,20 @@
 package com.example.runnershi_develop.viewholders
 
-import android.view.View
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.example.runnershi_develop.HomeViewPagerFragmentDirections
 import com.example.runnershi_develop.databinding.ListItemBadgeBinding
 import com.example.runnershi_develop.data.Badge
+import com.example.runnershi_develop.utilities.OnClickListener
 
-class BadgeViewHolder(private val binding: ListItemBadgeBinding) : RecyclerView.ViewHolder(binding.root) {
-
-    init{
-        binding.setClickListener { view ->
-            binding.badgeData?.let { badgeData ->
-                if(badgeData.badge)
-                    navigateToBadgeDetail(badgeData.index, view)
-            }
-        }
-    }
+class BadgeViewHolder(private val binding: ListItemBadgeBinding, private val onClick: (badge: Badge?)-> Unit) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(badgeBool: Boolean, index: Int) {
         with(binding) {
-            badgeData= Badge(
+            badgeData = Badge(
                 badgeBool,
                 index
             )
+            onClickListener = OnClickListener<Badge>(onClick)
             executePendingBindings()
         }
-    }
-
-    private fun navigateToBadgeDetail(index: Int, view: View) {
-        val direction = HomeViewPagerFragmentDirections
-            .actionViewPagerFragmentToFragmentBadgeDetail(index)
-        view.findNavController().navigate(direction)
     }
 }
