e4d188b78db595f05e770960636dc71f48a2feee
famer9716 <famer3625@gmail.com>
2020-09-19 00:35:14 +0900

    Add Opponent, My, DatabaseRunningDetail in RunningDetail.kt TODO : Ignore coordinate

--- app/src/main/java/com/example/runnershi_develop/data/RunningDetail.kt
+++ app/src/main/java/com/example/runnershi_develop/data/RunningDetail.kt
@@ -1,5 +1,9 @@
 package com.example.runnershi_develop.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.Ignore
 import com.google.gson.annotations.SerializedName
 
 data class RunningDetail(
@@ -8,9 +12,45 @@ data class RunningDetail(
     @SerializedName("day")
     val day: Int,
     @SerializedName("start_time")
+    @ColumnInfo(name = "start_time")
     val startTime: String,
     @SerializedName("end_time")
-    val endTime: String,
-    @SerializedName("coordinate")
-    val coordinate: List<Coordinate>
+    @ColumnInfo(name = "end_time")
+    val endTime: String
+//    @SerializedName("coordinate")
+//    @Ignore val coordinate: List<Coordinate>
+)
+
+data class MyRunningDetail(
+    @SerializedName("pace_minute")
+    @ColumnInfo(name = "pace_minute")
+    val paceMinute: Int,
+    @SerializedName("pace_second")
+    @ColumnInfo(name = "pace_second")
+    val paceSecond: Int
+)
+
+data class OpponentRunningDetail(
+    @SerializedName("nickname")
+    val nickname: String,
+    @SerializedName("distance")
+    val distance: Int,
+    @SerializedName("pace_minute")
+    @ColumnInfo(name = "pace_minute")
+    val paceMinute: Int,
+    @SerializedName("pace_second")
+    @ColumnInfo(name = "pace_second")
+    val paceSecond: Int
+)
+
+@Entity
+data class DataBaseRunningDetail(
+    @ColumnInfo(name = "run_idx")
+    val runIdx: Int,
+    @Embedded
+    val runningDetail: RunningDetail?,
+    @Embedded(prefix = "my_")
+    val myRunningDetail: MyRunningDetail?,
+    @Embedded(prefix = "opponent_")
+    val opponentRunningDetail: OpponentRunningDetail?
 )
