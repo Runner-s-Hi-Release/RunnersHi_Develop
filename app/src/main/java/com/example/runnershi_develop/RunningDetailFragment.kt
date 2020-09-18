b6ec1a7b57a6bb236ba8bf4a20a495dab620c2b2
famer9716 <famer3625@gmail.com>
2020-09-19 00:40:18 +0900

    Add RunningDetailFragment.kt

---
+++ app/src/main/java/com/example/runnershi_develop/RunningDetailFragment.kt
@@ -0,0 +1,35 @@
+package com.example.runnershi_develop
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.example.runnershi_develop.databinding.FragmentRunningDetailBinding
+import com.example.runnershi_develop.utilities.InjectorUtils
+import com.example.runnershi_develop.viewmodels.RunningDetailViewModel
+
+class RunningDetailFragment : Fragment() {
+
+    private val args: RunningDetailFragmentArgs by navArgs()
+
+    private val runningDetailViewModel: RunningDetailViewModel by viewModels {
+        InjectorUtils.provideRunningDetailViewModelFactory(requireActivity(), args.running.runIdx, args.running.gameIdx)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentRunningDetailBinding>(
+            inflater, R.layout.fragment_running_detail, container, false
+        ).apply{
+            viewModel = runningDetailViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        return binding.root
+    }
+}
