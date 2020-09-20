c0dd68c6fb414ef11333e922c24e1ebc55ed2062
famer9716 <famer3625@gmail.com>
2020-09-21 01:00:35 +0900

    Add NetworkRunningDetail to RunningDetail.kt

--- app/src/main/java/com/example/runnershi_develop/data/RunningDetail.kt
+++ app/src/main/java/com/example/runnershi_develop/data/RunningDetail.kt
@@ -3,23 +3,31 @@ package com.example.runnershi_develop.data
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.Ignore
 import com.google.gson.annotations.SerializedName
 
 data class RunningDetail(
-    @SerializedName("month")
     val month: Int,
-    @SerializedName("day")
     val day: Int,
-    @SerializedName("start_time")
     @ColumnInfo(name = "start_time")
     val startTime: String,
-    @SerializedName("end_time")
     @ColumnInfo(name = "end_time")
     val endTime: String
-//    @SerializedName("coordinate")
-//    @Ignore val coordinate: List<Coordinate>
 )
+
+data class NetworkRunningDetail(
+    val month: Int,
+    val day: Int,
+    @SerializedName("start_time")
+    val startTime: String,
+    @SerializedName("end_time")
+    val endTime: String,
+    @SerializedName("coordinate")
+    val coordinates: ArrayList<Coordinate>
+)
+
+fun NetworkRunningDetail.asRunningDetail(): RunningDetail{
+    return RunningDetail(this.month, this.day, this.startTime, this.endTime)
+}
 
 data class MyRunningDetail(
     @SerializedName("pace_minute")
