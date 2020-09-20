26a842d01b470a552a54024d4d44b09dce6d8d8b
famer9716 <famer3625@gmail.com>
2020-09-21 01:02:15 +0900

    Add naverFragment to RunningDetailFragment.kt

--- app/src/main/java/com/example/runnershi_develop/RunningDetailFragment.kt
+++ app/src/main/java/com/example/runnershi_develop/RunningDetailFragment.kt
@@ -1,14 +1,17 @@
 package com.example.runnershi_develop
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.runnershi_develop.data.Coordinate
 import com.example.runnershi_develop.databinding.FragmentRunningDetailBinding
 import com.example.runnershi_develop.utilities.InjectorUtils
 import com.example.runnershi_develop.viewmodels.RunningDetailViewModel
@@ -16,10 +19,10 @@ import com.example.runnershi_develop.viewmodels.RunningDetailViewModel
 class RunningDetailFragment : Fragment() {
 
     private val args: RunningDetailFragmentArgs by navArgs()
-
     private val runningDetailViewModel: RunningDetailViewModel by viewModels {
         InjectorUtils.provideRunningDetailViewModelFactory(requireActivity(), args.running.runIdx, args.running.gameIdx)
     }
+    private val naverMapFragment = NaverMapFragment()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -29,11 +32,19 @@ class RunningDetailFragment : Fragment() {
         ).apply{
             viewModel = runningDetailViewModel
             lifecycleOwner = viewLifecycleOwner
+
+            runningDetailViewModel.coordinates?.observe(viewLifecycleOwner, Observer {
+                it?.let{
+                    naverMapFragment.updateCoordinates(it)
+                }
+            })
+
+            btnRecDetailBack.setOnClickListener{view ->
+                view.findNavController().navigateUp()
+            }
         }
 
-        binding.btnRecDetailBack.setOnClickListener{view ->
-            view.findNavController().navigateUp()
-        }
+        childFragmentManager.beginTransaction().add(R.id.naver_map_layout, naverMapFragment, "TAG_PARENT").commit()
 
         return binding.root
     }
