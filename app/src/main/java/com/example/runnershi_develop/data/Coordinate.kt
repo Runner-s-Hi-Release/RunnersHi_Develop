af46621b11920f7b46e60dfdd2024280063262f8
famer9716 <famer3625@gmail.com>
2020-09-21 00:56:55 +0900

    Add Parcelize to Coordinate.kt

--- app/src/main/java/com/example/runnershi_develop/data/Coordinate.kt
+++ app/src/main/java/com/example/runnershi_develop/data/Coordinate.kt
@@ -1,10 +1,13 @@
 package com.example.runnershi_develop.data
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Coordinate(
     @SerializedName("latitude")
     val latitude: Double,
     @SerializedName("longitude")
     val longitude: Double
-)
+): Parcelable
