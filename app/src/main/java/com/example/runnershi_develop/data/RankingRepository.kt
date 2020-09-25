403cd01801c11097e00311bdf84b3216106e5e7a
chaeppy <chaeppy108@gmail.com>
2020-09-26 01:07:16 +0900

    add RankingRepository.kt 이달의러너, 명예의전당, 졌.잘.싸 api와 통신하는 저장소

---
+++ app/src/main/java/com/example/runnershi_develop/data/RankingRepository.kt
@@ -0,0 +1,59 @@
+package com.example.runnershi_develop.data
+
+import com.example.runnershi_develop.api.RequestToServer
+import com.example.runnershi_develop.api.ResultWrapper
+import com.example.runnershi_develop.api.safeApiCall
+
+class RankingRepository {
+    suspend fun getWinningRunner(token: String): List<RankingRunner.WinOrLoseRunner>? {
+        val callResult = safeApiCall {
+            RequestToServer
+                .service
+                .requestWinningRunner(token)
+        }
+        when (callResult) {
+            is ResultWrapper.Success -> {
+                return callResult.value.result
+            }
+        }
+        return null
+    }
+
+    suspend fun getLosingRunner(token: String): List<RankingRunner.WinOrLoseRunner>? {
+        val callResult = safeApiCall {
+            RequestToServer
+                .service
+                .requestLosingRunner(token)
+        }
+        when (callResult) {
+            is ResultWrapper.Success -> {
+                return callResult.value.result
+            }
+        }
+        return null
+    }
+
+    suspend fun getHeavyRunner(token: String): List<RankingRunner.HeavyRunner>? {
+        val callResult = safeApiCall {
+            RequestToServer
+                .service
+                .requestHeavyRunner(token)
+        }
+        when (callResult) {
+            is ResultWrapper.Success -> {
+                return callResult.value.result
+            }
+        }
+        return null
+    }
+
+    companion object {
+        @Volatile
+        private var instance: RankingRepository? = null
+
+        fun getInstance() =
+            instance ?: synchronized(this) {
+                instance ?: RankingRepository().also { instance = it }
+            }
+    }
+}
