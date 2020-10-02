ec5481a79068c574980c6b1dea8b71dc3d93870e
chaeppy <chaeppy108@gmail.com>
2020-10-02 21:10:08 +0900

    fix parameter in RankingBinding functions

--- app/src/main/java/com/example/runnershi_develop/adapters/RankingBinding.kt
+++ app/src/main/java/com/example/runnershi_develop/adapters/RankingBinding.kt
@@ -1,7 +1,5 @@
 package com.example.runnershi_develop.adapters
 
-import android.view.View.GONE
-import android.view.View.VISIBLE
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
@@ -11,38 +9,32 @@ import com.example.runnershi_develop.data.RankingRunner
 import com.example.runnershi_develop.extension.useGlide
 import com.example.runnershi_develop.viewmodels.RankingApiStatus
 
-@BindingAdapter("listData")
-fun RecyclerView.bindWinOrLoseRunnerRecyclerView(data: List<RankingRunner.WinOrLoseRunner>?) {
-    val adapter = adapter as RankingRunnerAdapter
-    adapter.submitList(data)
-}
-
-@BindingAdapter("listData")
-fun RecyclerView.bindHeavyRunnerRecyclerView(data: List<RankingRunner.HeavyRunner>?) {
-    val adapter = adapter as RankingRunnerAdapter
+@BindingAdapter("rankingList")
+fun bindWinOrLoseRunnerRecyclerView(view: RecyclerView, data: List<RankingRunner>?) {
+    val adapter = view.adapter as RankingRunnerAdapter
     adapter.submitList(data)
 }
 
 @BindingAdapter("scoreOrDistance")
-fun TextView.bindScoreOrDistance(item: RankingRunner) {
-    text = when (item) {
-        is RankingRunner.WinOrLoseRunner -> resources.getString(R.string.win_lose)
+fun bindScoreOrDistance(view: TextView, item: RankingRunner) {
+    view.text = when (item) {
+        is RankingRunner.WinOrLoseRunner -> view.resources.getString(R.string.win_lose)
             .format(item.win, item.lose)
-        is RankingRunner.HeavyRunner -> resources.getString(R.string.km)
+        is RankingRunner.HeavyRunner -> view.resources.getString(R.string.km)
             .format(String.format("%.2f", item.distance_sum / 1000.0))
     }
 }
 
 @BindingAdapter("nickName")
-fun TextView.bindNickName(item: RankingRunner) {
-    text = when (item) {
+fun bindNickName(view: TextView, item: RankingRunner) {
+    view.text = when (item) {
         is RankingRunner.WinOrLoseRunner -> item.nickname
         is RankingRunner.HeavyRunner -> item.nickname
     }
 }
 
 @BindingAdapter("profileImage")
-fun ImageView.bindProfileImage(item: RankingRunner) {
+fun bindProfileImage(view: ImageView, item: RankingRunner) {
     val itemImageIndex = when (item) {
         is RankingRunner.WinOrLoseRunner -> item.image
         is RankingRunner.HeavyRunner -> item.image
@@ -58,5 +50,5 @@ fun ImageView.bindProfileImage(item: RankingRunner) {
         8 -> R.drawable.icon_redwomen_shortmhair
         else -> R.drawable.icon_redwomen_bunhair
     }
-    useGlide(itemImage)
+    view.useGlide(itemImage)
 }
