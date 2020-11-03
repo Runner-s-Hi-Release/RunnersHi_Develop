59d06cdd42450c9d0b10c33d9897d4898fe131f7
chaeppy <chaeppy108@gmail.com>
2020-11-03 13:42:12 +0900

    add foundation of views for race feature

---
+++ app/src/main/java/com/example/runnershi_develop/RaceFragment.kt
@@ -0,0 +1,31 @@
+package com.example.runnershi_develop
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RaceFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RaceFragment()
+    }
+
+    private lateinit var viewModel: RaceViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.race_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(RaceViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/runnershi_develop/RaceViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.runnershi_develop
+
+import androidx.lifecycle.ViewModel
+
+class RaceViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
