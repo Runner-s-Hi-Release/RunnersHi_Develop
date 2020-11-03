f44841b00821101d5aae7ea4dcb80e7ee62952f1
chaeppy <chaeppy108@gmail.com>
2020-11-03 21:12:15 +0900

    add animation feature in RaceFragment.kt 이 코드는 MVVM을 적용하지 않은 코드입니다. 추후 수정 예정입니다.

--- app/src/main/java/com/example/runnershi_develop/RaceFragment.kt
+++ app/src/main/java/com/example/runnershi_develop/RaceFragment.kt
@@ -1,31 +1,60 @@
 package com.example.runnershi_develop
 
-import androidx.lifecycle.ViewModelProviders
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.runnershi_develop.databinding.FragmentRaceBinding
 
 class RaceFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = RaceFragment()
-    }
-
-    private lateinit var viewModel: RaceViewModel
+    private val raceViewModel: RaceViewModel = RaceViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_race, container, false)
-    }
+        val binding = FragmentRaceBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = this@RaceFragment
+        }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(RaceViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        var winnerMove = 0f
+        var loserMove = 0f
+        var winnerSrc = 0
+        var loserSrc = 0
 
+        binding.btnRunLock.setOnClickListener {
+            if (loserMove == 0f) {
+                loserMove = -280f
+                loserSrc = R.drawable.icon_run_win
+            } else {
+                loserMove = 0f
+                loserSrc = R.drawable.icon_run_lose
+            }
+
+            if (winnerMove == 0f) {
+                winnerMove = 280f
+                winnerSrc = R.drawable.icon_run_lose
+            } else {
+                winnerMove = 0f
+                winnerSrc = R.drawable.icon_run_win
+            }
+
+            ObjectAnimator.ofFloat(binding.raceLoserProfile, "translationY", loserMove).apply {
+                duration = 300
+                start()
+                binding.imgvLoserRaking.setImageResource(loserSrc)
+            }
+
+            ObjectAnimator.ofFloat(binding.raceWinnerProfile, "translationY", winnerMove)
+                .apply {
+                    duration = 300
+                    start()
+                    binding.imgvWinnerRaking.setImageResource(winnerSrc)
+                }
+        }
+
+        return binding.root
+    }
 }
