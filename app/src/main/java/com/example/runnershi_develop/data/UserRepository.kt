7ddfa51ab0fb4201282b0acd8d9798e36b14e384
famer9716 <famer3625@gmail.com>
2020-12-04 15:10:28 +0900

    Fix createUser in UserRepository.kt

--- app/src/main/java/com/example/runnershi_develop/data/UserRepository.kt
+++ app/src/main/java/com/example/runnershi_develop/data/UserRepository.kt
@@ -1,27 +1,27 @@
 package com.example.runnershi_develop.data
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import com.example.runnershi_develop.api.RequestToServer
 import com.example.runnershi_develop.api.ResultWrapper
 import com.example.runnershi_develop.api.safeApiCall
+import com.example.runnershi_develop.utilities.ACCESS_TOKEN
+import com.example.runnershi_develop.utilities.PrefInit
+import com.example.runnershi_develop.utilities.USER_ID
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class UserRepository private constructor(
     private val userDao: UserDao
 ) {
-    suspend fun createUser(token: String) {
-        val result = safeApiCall(call = {
-            RequestToServer
-                .service
-                .requestmyProfile(token)
-        })
+
+    suspend fun createUser() {
+        val result = safeApiCall(call = {RequestToServer.service.requestToken(UUID(PrefInit.prefs.getString(
+            USER_ID, "")))})
         withContext(Dispatchers.IO) {
             when (result) {
                 is ResultWrapper.Success -> {
-                    Log.d("result", result.value.result.nickname)
-                    userDao.insertUser(result.value.result)
+                    PrefInit.prefs.setString(ACCESS_TOKEN, result.value.data.accessToken)
+                    userDao.insertUser(result.value.data)
 
                 }
             }
