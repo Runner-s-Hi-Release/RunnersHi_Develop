7eb731de86b851d2f382e65c5caa87f66eb63804
famer9716 <famer3625@gmail.com>
2020-12-04 15:14:12 +0900

    Add SplashViewModel.kt

---
+++ app/src/main/java/com/example/runnershi_develop/viewmodels/SplashViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.runnershi_develop.viewmodels
+
+import androidx.lifecycle.*
+import com.example.runnershi_develop.data.UserRepository
+import kotlinx.coroutines.launch
+
+class SplashViewModel internal constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+    fun createUser() {
+        viewModelScope.launch {
+            userRepository.createUser()
+        }
+    }
+}
