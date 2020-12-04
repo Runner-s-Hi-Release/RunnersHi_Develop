137ed3da735e83b9118a7365f0df4aac92cc05ac
famer9716 <famer3625@gmail.com>
2020-12-04 15:18:30 +0900

    Add UUID, navigationToOnBoard in SplashFragment.kt

--- app/src/main/java/com/example/runnershi_develop/SplashFragment.kt
+++ app/src/main/java/com/example/runnershi_develop/SplashFragment.kt
@@ -6,11 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.runnershi_develop.databinding.FragmentSplashBinding
 import com.example.runnershi_develop.extension.animatorListener
+import com.example.runnershi_develop.utilities.*
+import com.example.runnershi_develop.viewmodels.SplashViewModel
+import java.util.*
 
 class SplashFragment : Fragment() {
+    private val splashViewModel: SplashViewModel by viewModels {
+        InjectorUtils.provideSplashViewModelFactory(requireContext())
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,11 +25,21 @@ class SplashFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentSplashBinding>(
             inflater,
             R.layout.fragment_splash, container, false
-        )
+        ).apply {
+            lifecycleOwner = this@SplashFragment
+        }
 
         binding.splashStart.animatorListener {
-            view?.findNavController()
-                ?.navigate(SplashFragmentDirections.actionSplashFragmentToHomeViewPagerFragment())
+            if (PrefInit.prefs.getString(FIRST_RUN, "y") == "y") {
+                PrefInit.prefs.setString(USER_ID, UUID.randomUUID().toString())
+                PrefInit.prefs.setString(FIRST_RUN, "n")
+                splashViewModel.createUser()
+                view?.findNavController()
+                    ?.navigate(SplashFragmentDirections.actionSplashFragmentToOnBoardFragment())
+            } else {
+                view?.findNavController()
+                    ?.navigate(SplashFragmentDirections.actionSplashFragmentToHomeViewPagerFragment())
+            }
         }
 
         return binding.root
