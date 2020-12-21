7e0f947553855fa4a96e36b0b711d9c5212cf906
famer9716 <famer3625@gmail.com>
2020-12-22 04:15:48 +0900

    Fix createUser in UserRepository.kt

--- app/src/main/java/com/example/runnershi_develop/data/UserRepository.kt
+++ app/src/main/java/com/example/runnershi_develop/data/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.example.runnershi_develop.data
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import com.example.runnershi_develop.api.RequestToServer
 import com.example.runnershi_develop.api.ResultWrapper
@@ -7,23 +8,30 @@ import com.example.runnershi_develop.api.safeApiCall
 import com.example.runnershi_develop.utilities.ACCESS_TOKEN
 import com.example.runnershi_develop.utilities.PrefInit
 import com.example.runnershi_develop.utilities.USER_ID
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 
 class UserRepository private constructor(
     private val userDao: UserDao
 ) {
 
     suspend fun createUser() {
-        val result = safeApiCall(call = {RequestToServer.service.requestToken(UUID(PrefInit.prefs.getString(
-            USER_ID, "")))})
-        withContext(Dispatchers.IO) {
-            when (result) {
-                is ResultWrapper.Success -> {
-                    PrefInit.prefs.setString(ACCESS_TOKEN, result.value.data.accessToken)
-                    userDao.insertUser(result.value.data)
-
-                }
+        when (val callResult = safeApiCall {
+            RequestToServer.service
+                .requestToken(UUID(PrefInit.prefs.getString(USER_ID, "")))
+        }) {
+            is ResultWrapper.Success -> {
+                PrefInit.prefs.setString(ACCESS_TOKEN, callResult.value.data.accessToken)
+                userDao.insertUser(callResult.value.data)
+                Log.d("Running Repository", "Success")
+            }
+            is HttpException -> {
+                Log.d("Running Repository", "Call Result: Http Exception")
+            }
+            is ResultWrapper.NetworkError -> {
+                Log.d("Running Repository", "Call Result: Network Error")
+            }
+            is ResultWrapper.GenericError -> {
+                Log.d("Running Repository", "Call Result: Generic Error")
             }
         }
     }
