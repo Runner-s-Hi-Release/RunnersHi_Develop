bfc3c4e042fdf24365890f7817e6948920db11c7
famer9716 <famer3625@gmail.com>
2020-12-22 04:17:41 +0900

    Add timer to MatchingViewModel.kt

--- app/src/main/java/com/example/runnershi_develop/viewmodels/MatchingViewModel.kt
+++ app/src/main/java/com/example/runnershi_develop/viewmodels/MatchingViewModel.kt
@@ -3,11 +3,15 @@ package com.example.runnershi_develop.viewmodels
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.runnershi_develop.data.MatchingRepository
+import com.example.runnershi_develop.data.Opponent
 import com.example.runnershi_develop.data.RunningStart
 import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.concurrent.timerTask
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MatchingViewModel internal constructor(
@@ -15,8 +19,12 @@ class MatchingViewModel internal constructor(
     private val runningStart: RunningStart
 ) : ViewModel() {
 
-    val success: LiveData<Boolean>
-        get() = matchingRepository.getSuccess()
+    private var timer = Timer()
+
+    val opponent: LiveData<Opponent>
+        get() = matchingRepository.getOpponent()
+
+    val matchingTime: MutableLiveData<Int> = MutableLiveData()
 
     fun startForegroundService(){
         viewModelScope.launch{
@@ -26,5 +34,19 @@ class MatchingViewModel internal constructor(
 
     fun stopForegroundService(){
         matchingRepository.stopForegroundService()
+    }
+
+    private fun stopTimerTask() {
+        timer?.cancel()
+        timer?.purge()
+        timer = Timer()
+    }
+
+    fun startTimerTask() {
+        stopTimerTask()
+        matchingTime.value = 0
+        timer.schedule(timerTask{
+            matchingTime.postValue(matchingTime.value!! + 1)
+        }, 0, 1000)
     }
 }
