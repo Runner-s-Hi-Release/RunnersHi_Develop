dc8ff327853847e6259469aee145e985335b721c
famer9716 <famer3625@gmail.com>
2020-12-22 04:16:34 +0900

    Add stopMatching, confirmRunning, requestRunningStart to ForegroundService.kt

--- app/src/main/java/com/example/runnershi_develop/service/ForegroundService.kt
+++ app/src/main/java/com/example/runnershi_develop/service/ForegroundService.kt
@@ -5,60 +5,105 @@ import android.content.Intent
 import android.os.*
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.navigation.NavDeepLinkBuilder
 import com.example.runnershi_develop.MainActivity
 import com.example.runnershi_develop.R
 import com.example.runnershi_develop.api.RequestToServer
 import com.example.runnershi_develop.api.ResultWrapper
 import com.example.runnershi_develop.api.safeApiCall
+import com.example.runnershi_develop.data.Opponent
+import com.example.runnershi_develop.data.ResponseData
 import com.example.runnershi_develop.data.RunningStart
 import com.example.runnershi_develop.utilities.ACCESS_TOKEN
 import com.example.runnershi_develop.utilities.PrefInit
 import kotlinx.coroutines.*
 import retrofit2.HttpException
 import java.util.*
-import kotlin.concurrent.timerTask
 
 class ForegroundService internal constructor() : Service(){
 
-    var index = 0
-    fun requestRunningStart(intent: Intent, receiver: ResultReceiver){
-        CoroutineScope(Dispatchers.Main + SupervisorJob()).launch {
-
-            when (val callResult = safeApiCall {
-                RequestToServer.service
-                    .requestRunningStart(PrefInit.prefs.getString(ACCESS_TOKEN, ""),
-                        intent.extras?.getSerializable("runningStart") as RunningStart)
-            }) {
-                is ResultWrapper.Success -> {
-                    index += 1
-                    val bundle = Bundle()
-                    bundle.putBoolean("success", callResult.value.success)
-                    receiver.send(Activity.RESULT_OK, bundle)
-                }
-                is HttpException -> {
-                    Log.d("Running Repository", "Call Result: Http Exception")
-                    //todo response 가 200아닐시 무엇을 할지 논의 필요
-                }
-                is ResultWrapper.NetworkError -> {
-                    Log.d("Running Repository", "Call Result: Network Error")
-                    // todo 통신 실패시 무엇을 할지 논의 필요
-                }
-                is ResultWrapper.GenericError -> {
-                    Log.d("Running Repository", "Call Result: Generic Error")
-                    //todo
-                }
+    private suspend fun stopMatching(){
+        when (val callResult = safeApiCall {
+            RequestToServer.service
+                .requestStopMatching(PrefInit.prefs.getString(ACCESS_TOKEN, ""))
+        }) {
+            is ResultWrapper.Success -> {
+                stopForegroundService()
+                Log.d("Running Repository", "Success")
+            }
+            is HttpException -> {
+                Log.d("Running Repository", "Call Result: Http Exception")
+            }
+            is ResultWrapper.NetworkError -> {
+                Log.d("Running Repository", "Call Result: Network Error")
+            }
+            is ResultWrapper.GenericError -> {
+                Log.d("Running Repository", "Call Result: Generic Error")
             }
         }
     }
 
 
-    val CHANNEL_ID = "ForegroundServiceChannel"
+    private suspend fun confirmRunning(intent: Intent, receiver: ResultReceiver){
+        when (val callResult = safeApiCall {
+            RequestToServer.service
+                .requestConfirm(PrefInit.prefs.getString(ACCESS_TOKEN, ""))
+        }) {
+            is ResultWrapper.Success -> {
+                when(callResult.value.status){
+                    200 -> {
+                        val bundle = Bundle()
+                        bundle.putParcelable("opponent", (callResult as ResultWrapper.Success<ResponseData<Opponent>>).value.data)
+                        receiver.send(Activity.RESULT_OK, bundle)
+                        stopForegroundService()
+                    }
+                    202 -> requestRunningStart(intent, receiver)
+                }
+            }
+            is HttpException -> {
+                Log.d("Running Repository", "Call Result: Http Exception")
+            }
+            is ResultWrapper.NetworkError -> {
+                Log.d("Running Repository", "Call Result: Network Error")
+            }
+            is ResultWrapper.GenericError -> {
+                Log.d("Running Repository", "Call Result: Generic Error")
+            }
+        }
+    }
+
+
+    private suspend fun requestRunningStart(intent: Intent, receiver: ResultReceiver){
+        when (val callResult = safeApiCall {
+            RequestToServer.service
+                .requestRunningFind(PrefInit.prefs.getString(ACCESS_TOKEN, ""),
+                    intent.extras?.getSerializable("runningStart") as RunningStart)
+        }) {
+            is ResultWrapper.Success -> {
+                when(callResult.value.success){
+                    true -> {
+                        confirmRunning(intent, receiver)
+                    }
+                    false -> {
+                        requestRunningStart(intent, receiver)
+                    }
+                }
+            }
+            is HttpException -> {
+                Log.d("Running Repository", "Call Result: Http Exception")
+            }
+            is ResultWrapper.NetworkError -> {
+                Log.d("Running Repository", "Call Result: Network Error")
+            }
+            is ResultWrapper.GenericError -> {
+                Log.d("Running Repository", "Call Result: Generic Error")
+            }
+        }
+    }
+
+
+    private val CHANNEL_ID = "ForegroundServiceChannel"
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        if(timer == null){
-            timer = Timer()
-        }
         when (intent.action) {
             ACTION_START_FOREGROUND_SERVICE -> {
                 createNotificationChannel()
@@ -68,28 +113,25 @@ class ForegroundService internal constructor() : Service(){
                     }
                 val notification: Notification = Notification.Builder(this, CHANNEL_ID)
                     .setContentTitle("매칭중")
-                    .setSmallIcon(com.example.runnershi_develop.R.drawable.match_logo)
+                    .setSmallIcon(R.drawable.match_logo)
                     .setContentIntent(pendingIntent)
                     .setTicker("ticker test")
                     .build()
                 startForeground(1, notification)
 
-
-                timer?.schedule(timerTask {
-                    intent.extras?.getParcelable<ResultReceiver>("receiver")?.let {
+                intent.extras?.getParcelable<ResultReceiver>("receiver")?.let {
+                    CoroutineScope(Dispatchers.Main + SupervisorJob()).launch {
                         requestRunningStart(intent,
-                            it
+                        it
                         )
                     }
-                }, 0, 1000)
-
+                }
 
             }
             ACTION_STOP_FOREGROUND_SERVICE -> {
-                timer?.cancel()
-                timer?.purge()
-                timer = null
-                stopForegroundService()
+                CoroutineScope(Dispatchers.Main + SupervisorJob()).launch {
+                    stopMatching()
+                }
             }
         }
 
@@ -118,6 +160,5 @@ class ForegroundService internal constructor() : Service(){
     companion object{
         const val ACTION_START_FOREGROUND_SERVICE = "ACTION_START_FOREGROUND_SERVICE"
         const val ACTION_STOP_FOREGROUND_SERVICE = "ACTION_STOP_FOREGROUND_SERVICE"
-        var timer: Timer? = null
     }
 }
