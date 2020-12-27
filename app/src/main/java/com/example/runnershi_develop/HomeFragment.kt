4ad5ecc86322fdd0ef5985332f9b485fc6096115
famer9716 <famer3625@gmail.com>
2020-12-28 00:13:47 +0900

    Add permission check in HomeFragment.kt

--- app/src/main/java/com/example/runnershi_develop/HomeFragment.kt
+++ app/src/main/java/com/example/runnershi_develop/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.runnershi_develop
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.runnershi_develop.databinding.FragmentHomeBinding
+import com.example.runnershi_develop.utilities.PermissionHandler
 
 
 class HomeFragment : Fragment() {
@@ -16,14 +18,29 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentHomeBinding.inflate(inflater, container, false).apply{
+            lifecycleOwner = this@HomeFragment
             btnHomeRunnow.setOnClickListener {
-                findNavController().navigate(
-                    HomeViewPagerFragmentDirections
-                        .actionHomeViewPagerFragmentToMatchingGoalFragment()
+
+                val ifPermissionGranted = PermissionHandler.checkPermission(
+                    requireActivity(),
+                    listOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    REQUEST_LOCATION_PERMISSION
                 )
+                if(ifPermissionGranted) {
+                    findNavController().navigate(
+                        HomeViewPagerFragmentDirections
+                            .actionHomeViewPagerFragmentToMatchingGoalFragment()
+                    )
+                }
             }
         }
         return binding.root
+    }
+    companion object{
+        private const val REQUEST_LOCATION_PERMISSION = 1000
     }
 }
 
